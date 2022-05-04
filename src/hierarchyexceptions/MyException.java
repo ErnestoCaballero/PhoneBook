2bd2cb92d467981d8e601e8ace95912c66654e83
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-05-03 23:27:20 -0500

    add MyException class to send a customer exception message to the super class Exception

--- src/hierarchyexceptions/MyException.java
+++ src/hierarchyexceptions/MyException.java
@@ -1,5 +1,15 @@
 package hierarchyexceptions;
 
-public class MyException {
-    
+/*
+BASE CLASS
+Create a class named MyException, which must inherit from the base class for all exceptions and errors.
+Inside the MyException class declare a constructor that takes String message as an argument and passes the message to the superclass.
+ */
+
+public class MyException extends Exception {
+
+    public MyException(String message) {
+        super(message);
+    }
+
 }
