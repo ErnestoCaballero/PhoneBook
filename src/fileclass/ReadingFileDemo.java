fef7722a851023a8521bfc57d2d9ef31073b618d
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-05-06 16:44:10 -0500

    add readFileAsString(String filename) method to read a java file and output on tha main method catching the exception#

---
+++ src/fileclass/ReadingFileDemo.java
@@ -0,0 +1,23 @@
+package fileclass;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ReadingFileDemo {
+
+    public static void main(String[] args) {
+        String pathToHelloWorldJava = "/Users/ernestocaballero/Documents/JetbrainsAcademy/JavaDeveloperTrack/PhoneBook/" +
+                "TestFiles/HelloWorld/HelloWorld.java";
+
+        try {
+            System.out.println(readFileAsString(pathToHelloWorldJava));
+        } catch (IOException e) {
+            System.out.println("Cannot read file: " + e.getMessage());
+        }
+    }
+
+    public static String readFileAsString(String filename) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filename)));
+    }
+}
