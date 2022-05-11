a9782cc732055ae1a49dcd9054bcc1f1e9dfe858
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-05-11 03:37:48 -0500

    create a new file using the printWriter class and ask for a number on standard input to add to the file

--- src/writerclasses/MyFileWriter.java
+++ src/writerclasses/MyFileWriter.java
@@ -3,6 +3,8 @@ package writerclasses;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class MyFileWriter {
 
@@ -14,6 +16,19 @@ public class MyFileWriter {
             writer.write("Hello, Ernesto\nYou're a great Java programmer!");
         } catch (IOException e) {
             System.out.println("IOException! " + e.getMessage());
+        }
+
+        File file1 = new File("./TestFiles/WritingFiles/firstPrintWriter.txt");
+
+        try (PrintWriter printWriter = new PrintWriter(file1)) {
+            printWriter.println("From PrintWriter class");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Hey, tell me which number to write in the file: ");
+            int number = scanner.nextInt();
+            printWriter.printf("The number you wrote was %d%n", number);
+            printWriter.printf("Great, you've write into this file!");
+        } catch (IOException e) {
+
         }
     }
 }
