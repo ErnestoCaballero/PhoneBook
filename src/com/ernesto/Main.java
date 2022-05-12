e95f11cc67abe313dda3d02106426022fd8f88b4
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-05-12 14:27:27 -0500

    add time lapse to main method

--- src/com/ernesto/Main.java
+++ src/com/ernesto/Main.java
@@ -1,20 +1,116 @@
 package com.ernesto;
 
+import jdk.swing.interop.SwingInterOpUtils;
 import superkey.Developer;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        File smallFind = new File("./TestFiles/StageOne/small_find.txt");
+        File smallDirectory = new File("./TestFiles/StageOne/small_directory.txt");
+        System.out.println("Start searching...");
+        long start = System.currentTimeMillis();
 
-        System.out.println(File.separator);
-        System.out.println(Integer.MAX_VALUE);
+        String[] toFind = getInputArray(smallFind);
+
+        int found = 0;
+
+        try (Scanner scanner = new Scanner(smallDirectory)) {
+            String currentName;
+            while (scanner.hasNextLine()) {
+                scanner.next();
+                currentName = scanner.nextLine().trim();
+                if (elementExists(toFind, currentName)) {
+                    found++;
+                }
+                if (found == toFind.length) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println();
+        }
+
+        long end = System.currentTimeMillis();
+        long lapse = end - start;
+        long minutes = lapse / 1_000 / 60;
+        long seconds = (lapse / 1_000) % 60;
+        long ms = lapse % 1000;
+
+        System.out.printf("Found %d/%d entries. Time taken: %d min. %d sec. %d ms.%n", found, toFind.length, minutes, seconds , ms);
+
     }
 
-    public static void readFile() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("file1.txt"));
+    public static String[] getInputArray(File input) {
+        int countLines = 0;
+
+        try (Scanner scanner = new Scanner(input); Scanner scanner1 = new Scanner(input)) {
+            while (scanner.hasNextLine()) {
+                scanner.nextLine();
+                countLines++;
+            }
+
+            String[] toFind = new String[countLines];
+            int i = 0;
+
+            while (scanner1.hasNextLine()) {
+                toFind[i] = scanner1.nextLine();
+                i++;
+            }
+
+            return toFind;
+
+        } catch (IOException e) {
+            System.out.println();
+        }
+
+        return null;
+    }
+
+    static boolean elementExists(String[] arr, String toCheck) {
+        for (String s : arr) {
+            if (s.equals(toCheck)) {
+                removeElementFromArray(arr, toCheck);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String[] removeElementFromArray(String[] toReplace, String toRemove) {
+        boolean contains = false;
+
+        for (String s : toReplace) {
+            if (s.equals(toRemove)) {
+                contains = true;
+            }
+        }
+
+        if (!contains) {
+            System.out.println("The element doesn't exist in the Array!\nReturning original Array");
+            return toReplace;
+        }
+
+        String[] result = new String[toReplace.length - 1];
+
+        for (int i = 0; i < toReplace.length - 1; i++) {
+            if (!toReplace[i].equals(toRemove)) {
+                result[i] = toReplace[i];
+            } else {
+                for (int j = i; j < toReplace.length - 1; j++) {
+                    result[j] = toReplace[j + 1];
+                }
+                break;
+            }
+        }
+
+        result[result.length - 1] = toReplace[toReplace.length - 1];
+
+        return result;
     }
 }
--- src/com/ernesto/TestClass.java
+++ src/com/ernesto/TestClass.java
@@ -1,2 +1,68 @@
-package com.ernesto;public class TestClass {
+package com.ernesto;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Starting...");
+        long start = System.currentTimeMillis();
+        try {
+            Thread.sleep(3500);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+        long end = System.currentTimeMillis();
+        long lapse = end - start;
+
+        long minutes = lapse / 1_000 / 60;
+        long seconds = (lapse / 1_000) % 60;
+        long ms = lapse % 1000;
+//        double ms = lapse - (minutes * 60 * 1_000) - (seconds * 1_000 + ()
+        System.out.println("Minutes: " + minutes + " | seconds: " + seconds  + " | ms: " + ms);
+
+        System.out.println(end - start);
+        System.out.println(lapse);
+
+        System.out.println();
+
+        long time = 125_301;
+        long minutes1 = time / 1_000 / 60;
+        long seconds1 = (time / 1_000) % 60;
+        long ms1 = time % 1000;
+        System.out.println("Minutes: " + minutes1 + " | seconds: " + seconds1  + " | ms: " + ms1);
+
+
+    }
+
+    public static String[] removeElementFromArray(String[] toReplace, String toRemove) {
+        boolean contains = false;
+
+        for (String s : toReplace) {
+            if (s.equals(toRemove)) {
+                contains = true;
+            }
+        }
+
+        if (!contains) {
+            System.out.println("The element doesn't exist in the Array!\nReturning original Array");
+            return toReplace;
+        }
+
+        String[] result = new String[toReplace.length - 1];
+
+        for (int i = 0; i < toReplace.length - 1; i++) {
+            if (!toReplace[i].equals(toRemove)) {
+                result[i] = toReplace[i];
+            } else {
+                for (int j = i; j < toReplace.length - 1; j++) {
+                    result[j] = toReplace[j + 1];
+                }
+                break;
+            }
+        }
+
+        result[result.length - 1] = toReplace[toReplace.length - 1];
+
+        return result;
+    }
 }
