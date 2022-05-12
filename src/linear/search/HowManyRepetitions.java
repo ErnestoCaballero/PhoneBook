0a8d91cd8eadeb7f3e0f0262d31d4369624bfe1b
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-05-12 00:35:07 -0500

    overload method countRepetitions() so that it taked the reference element from the input file as per exercise instructions

--- src/linear/search/HowManyRepetitions.java
+++ src/linear/search/HowManyRepetitions.java
@@ -9,13 +9,39 @@ public class HowManyRepetitions {
     public static void main(String[] args) {
         File file = new File("./TestFiles/DataSets/countRepetitions.txt");
 
-        System.out.println(countRepetitions(file, "7"));
+//        System.out.println(countRepetitions(file, "7"));
+
+        System.out.println(countRepetitions(file));
+
+
 
     }
 
     static int countRepetitions(File file, String ref) {
         int count = 0;
         try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNext()) {
+                if (scanner.next().equals(ref)) {
+                    count++;
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("IOException occur! " + e);
+        }
+
+        return count;
+    }
+
+    /*
+    Reads the first element of the file as the element to check for occurences,
+    then counts the repetitions on nextLine
+     */
+
+    static int countRepetitions(File file) {
+        int count = 0;
+        try (Scanner scanner = new Scanner(file)) {
+            String ref = scanner.nextLine();
             while (scanner.hasNext()) {
                 if (scanner.next().equals(ref)) {
                     count++;
