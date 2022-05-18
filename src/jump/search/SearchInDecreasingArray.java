1daa7f6d6ab96633311d8c5d7ee9190995cc0baa
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-05-18 16:45:48 -0500

    add SearchInDecreasingArray class to test method getStringArrayFromFile()

--- src/jump/search/SearchInDecreasingArray.java
+++ src/jump/search/SearchInDecreasingArray.java
@@ -1,2 +1,21 @@
-package jump.search;public class SearchInDecreasingArray {
+package jump.search;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SearchInDecreasingArray {
+
+    public static String[] getStringArrayFromFile(File file, int lineNumber) throws IOException {
+        Scanner scanner = new Scanner(file);
+        String[] arr;
+
+        for (int i = 0; i < lineNumber - 1; i++) {
+            scanner.nextLine();
+        }
+
+        arr = scanner.nextLine().split(" ");
+
+        return arr;
+    }
 }
