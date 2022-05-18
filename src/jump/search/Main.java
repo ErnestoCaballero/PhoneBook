cadfac7afb4d6d291cc771d86c460bf501b98b37
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-05-18 02:05:39 -0500

    add geeksJumpSearch() and jumpSearch() methods

--- src/jump/search/Main.java
+++ src/jump/search/Main.java
@@ -4,10 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         int[] arr = {10, 13, 19, 20, 24, 26, 30, 34, 37};
-        int value = 36;
+        int value = 39;
 
 //        System.out.println(jumpSearch(arr, value));
-        System.out.printf("Result %d", myJumpSearch(arr, value));
+//        System.out.printf("Result %d", myJumpSearch(arr, value));
+        System.out.println(geeksJumpSearch(arr, value));
 
     }
 
@@ -51,14 +52,14 @@ public class Main {
 
     public static int jumpSearch(int[] arr, int value) {
         int step = (int) Math.floor(Math.sqrt(arr.length));
+        int curr = 1;
 
-        int curr = 0;
-
-        while (curr < arr.length) {
+        while (curr <= arr.length) {
             if (arr[curr] == value) {
                 return curr;
             } else if (arr[curr] > value) {
                 int ind = curr - 1;
+
                 while (ind > curr - step && ind >= 1) {
                     if (arr[ind] == value) {
                         return ind;
@@ -77,6 +78,32 @@ public class Main {
                 return ind;
             }
             ind--;
+        }
+        return -1;
+    }
+
+    public static int geeksJumpSearch(int[] arr, int value) {
+        int n = arr.length;
+        int step = (int) Math.floor(Math.sqrt(arr.length));
+        int prev = 0;
+
+        while (arr[Math.min(step, n) - 1] < value) {
+            prev = step;
+            step += (int) Math.floor(Math.sqrt(n));
+            if (prev >= n) {
+                return -1;
+            }
+        }
+
+        while (arr[prev] < value) {
+            prev++;
+            if (prev == Math.min(step, n)) {
+                return -1;
+            }
+        }
+
+        if (arr[prev] == value) {
+            return prev;
         }
 
         return -1;
