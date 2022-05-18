9c8bc28ba18710d75f77f26645fdc1665b3f7251
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-05-18 16:46:20 -0500

    complete Search in decreasing array exercise

--- src/jump/search/Main.java
+++ src/jump/search/Main.java
@@ -1,16 +1,46 @@
 package jump.search;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
-//        int[] arr = {10, 13, 19, 20, 24, 26, 30, 34, 37};
-//        int value = 39;
-//
-//        System.out.println(jumpSearch(arr, value));
-//        System.out.printf("Result %d", myJumpSearch(arr, value));
-//        System.out.println(geeksJumpSearch(arr, value));
+
+        File file = new File("./TestFiles/JumpSearch/dataset_searchDecreasing.txt");
+
+        try {
+            String[] inputOne = getStringArrayFromFile(file, 1);
+            String[] inputTwo = getStringArrayFromFile(file, 2);
+
+            int[] arr = new int[inputOne.length];
+            int[] toFind = new int[inputTwo.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = Integer.parseInt(inputOne[i]);
+            }
+            for (int i = 0; i < toFind.length; i++) {
+                toFind[i] = Integer.parseInt(inputTwo[i]);
+            }
+
+            Arrays.sort(arr);
+            Arrays.sort(toFind);
+
+            for (int i : toFind) {
+//                System.out.printf("Search for %d in toFind in arr: %d%n", i, myJumpSearch(arr, i) == -1 ? -1 : arr.length - myJumpSearch(arr, i) - 1);
+                System.out.printf("%d ", myJumpSearch(arr, i) == -1 ? -1 : arr.length - myJumpSearch(arr, i) - 1);
+            }
 
 
+        } catch (IOException e) {
+            System.out.printf("%s %s", "The file might not exists", e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("The data trying to be converted is not compatible! " + e.getMessage());
+        }
 
     }
 
@@ -108,5 +138,18 @@ public class Main {
         }
 
         return -1;
+    }
+
+    public static String[] getStringArrayFromFile(File file, int lineNumber) throws IOException {
+        Scanner scanner = new Scanner(file);
+        String[] arr;
+
+        for (int i = 0; i < lineNumber - 1; i++) {
+            scanner.nextLine();
+        }
+
+        arr = scanner.nextLine().split(" ");
+
+        return arr;
     }
 }
