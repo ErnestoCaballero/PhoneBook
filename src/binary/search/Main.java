8faaf7f6122cc168cf7a09b1c2c3da3199de5b8c
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-06-10 22:22:52 -0500

    add myBinarySearch() method to Main class

--- src/binary/search/Main.java
+++ src/binary/search/Main.java
@@ -1,5 +1,29 @@
 package binary.search;
 
 public class Main {
-    
+
+    public static void main(String[] args) {
+        int[] arr = {10, 13, 19, 20, 24, 26, 30, 34, 35, 40};
+        System.out.println(myBinarySearch(arr, 34));
+
+    }
+
+    public static int myBinarySearch(int[] arr, int tgt) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+
+            if (arr[middle] == tgt) {
+                return middle;
+            } else if (tgt > arr[middle]) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        return -1;
+    }
 }
